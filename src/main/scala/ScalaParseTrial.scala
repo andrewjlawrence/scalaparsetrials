ada3e25da957fd9e42522e5997afc06dddc6625f
ajlawrence <ajlawrence@Acm.org>
2016-02-25 20:45:34 +0000

    Get source indexes from parsers.

--- src/main/scala/ScalaParseTrial.scala
+++ src/main/scala/ScalaParseTrial.scala
@@ -3,6 +3,8 @@ import fastparse.all._
 import fastparse.parsers.Intrinsics
 
 
+case class TermName(start: Int, length: Int)
+
 abstract class Tree	
 {
 	def printValue(): Unit = {
@@ -10,12 +12,20 @@ abstract class Tree
 	}
 }
 
-case class Literal(value: String) extends Tree
+case class Literal(value: Int) extends Tree
 {
 	override def printValue(): Unit = {
 		println("Literal contains value: " + value)
 	}
 }
+
+case class Ident(name: TermName) extends Tree
+{
+	override def printValue(): Unit = {
+		println("Ident contains name start index : " + name.start + " and length " + name.length)
+	}
+}
+
 
 object ScalaParseTrial extends Literals {
   
@@ -28,11 +38,11 @@ object ScalaParseTrial extends Literals {
 	}
 
 	def parseDigit(string: String): Unit = {
-		val parser = P(Literals.Int.!.map(Literal))
+		val parser = P( "{" ~ Index ~ Literals.Int ~  Index ~ "}").map( (x : Tuple2[Int,Int]) => TermName(x._1, x._2 - x._1)).map(Ident)
 		val result = parser.parse(string)
 		result match {
 			case Parsed.Success(value, successIndex) =>
-				println("Parsed a digit with value: " + value.value + " and success index " + successIndex)
+				value.printValue()
 			case Parsed.Failure(p,i,e) =>
 				println("Parsing failed")
 		}   
@@ -43,7 +53,7 @@ object ScalaParseTrial extends Literals {
     	// Should succeed
 		parseDigit("a")
 		// Should fail
-		parseDigit("1")
+		parseDigit("{1}")
 		// Should succeed
 		parseDigit("a1")
     }
